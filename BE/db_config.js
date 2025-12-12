0cbcdc61745e715207c56b96f180e4aac07dc75b
NCTPhu05 <23050109@student.bdu.edu.vn>
2025-12-12 10:58:04 +0700

    Update database configuration for RDS MySQL

--- BE/db_config.js
+++ BE/db_config.js
@@ -1,12 +1,13 @@
 // db_config.js
 module.exports = {
     // **QUAN TRỌNG: Thay thế bằng Endpoint RDS MySQL của bạn**
-    host: 'tuyensinhdb.czk86e4ggm4t.ap-southeast-2.rds.amazonaws.com', 
+    host: 'db-1.cn64es6a2snr.ap-northeast-3.rds.amazonaws.com', 
     user: 'admin',      // User name bạn tạo khi tạo RDS
-    password: 'Hai24122004!', // Password bạn tạo khi tạo RDS
-    database: 'tuyensinhdb', // Tên Database
+    password: '123456789!', // Password bạn tạo khi tạo RDS
+    database: 'db-', // Tên Database
     port: 3306
 };
+
 
 
 
